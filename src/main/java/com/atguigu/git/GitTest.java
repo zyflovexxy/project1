61f4f151ab38172a5e77c9dc569f1f2582a006f5
12345 <269572276@qq.com>
2023-05-03 20:40:33 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello,git4");
         System.out.println("master test");
         System.out.println("hello,git5");
+        System.out.println("hello,git123");
     }
 }
